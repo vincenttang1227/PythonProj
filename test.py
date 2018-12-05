a383b47abbd84e6393e468e4fdf2754059bc3602
vincenttang1227 <vincent.tang@msa.hinet.net>
2018-12-05 17:29:31 +0800

    20181205 Python課程

--- test.py
+++
@@ -1,9 +0,0 @@
-nameDict = {}
-
-dictKey = input('Key: ')
-while dictKey != 'end':
-    dictValue = input('Value: ')
-    nameDict[dictKey] = dictValue
-    dictKey = input('Key: ')
-    
-print (input('Search key: ') in nameDict)
